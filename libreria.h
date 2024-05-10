d5b3a93d3d12f3fa30a5ea977a0b082dfd06b65a
AlejandraMiLara <a365544@uabc.edu.mx>
2024-05-10 00:11:00 -0700

    beta complete

--- libreria.h
+++ libreria.h
@@ -1,17 +1,16 @@
-#define RESET_COLOR    "\x1b[0m"
-#define NEGRO_T        "\x1b[30m"
-#define NEGRO_F        "\x1b[40m"
-#define ROJO_T     "\x1b[31m"
-#define ROJO_F     "\x1b[41m"
-#define MAGENTA_T        "\x1b[35m"
-#define MAGENTA_F        "\x1b[45m"
+#define RESET_COLOR "\x1b[0m"
+#define NEGRO_T "\x1b[30m"
+#define NEGRO_F "\x1b[40m"
+#define ROJO_T "\x1b[31m"
+#define ROJO_F "\x1b[41m"
+#define MAGENTA_T "\x1b[35m"
+#define MAGENTA_F "\x1b[45m"
 #define AMARILLO_T "\x1b[33m"
-#define    AMARILLO_F  "\x1b[43m"
-#define AZUL_T     "\x1b[34m"
-#define    AZUL_F      "\x1b[44m"
-#define BLANCO_T   "\x1b[37m"
-#define BLANCO_F   "\x1b[47m"
-
+#define AMARILLO_F "\x1b[43m"
+#define AZUL_T "\x1b[34m"
+#define AZUL_F "\x1b[44m"
+#define BLANCO_T "\x1b[37m"
+#define BLANCO_F "\x1b[47m"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,33 +21,24 @@
 #include <ctype.h>
 #include <unistd.h>
 
-/* PROTOTYPES */
+/* PROTOTIPOS */
 int msges();
 void menu();
 
 void go_to_XY(int x, int y);
-void text_to_XY(int x, int y, char *Text);
-void specialchar_to_XY(int x, int y, int Code);
+void text_to_XY(int x, int y, char *Texto);
+void specialchar_to_XY(int x, int y, int Codigo);
 
-void draw_title();
-void draw_wasd();
-void draw_color(int letter_code);
+void dibujar_titulo();
+void dibujar_botones();
+void dibujar_color(int cod);
 
-void cls();
+void juego();
+int aleat();
 
-void gameover(int status);
-void play(int vector[], int points);
-void heart();
+/* FUNCIONES */
 
-
-
-/* FUNCTIONS*/
-
- /////////////////////////////
-/////// GO TO'S FUNCTIOS //////////
-  //////////////////////////////
-
-void go_to_XY(int x, int y) 
+void go_to_XY(int x, int y)
 {
     HANDLE hcon;
     hcon = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -58,264 +48,263 @@ void go_to_XY(int x, int y)
     SetConsoleCursorPosition(hcon, dwPos);
 }
 
-void text_to_XY(int x, int y, char *Text) 
+void text_to_XY(int x, int y, char *Texto)
 {
     go_to_XY(x, y);
-    printf("%s", Text);
+    printf("%s", Texto);
 }
 
-void specialchar_to_XY(int x, int y, int Code) 
+void specialchar_to_XY(int x, int y, int Codigo)
 {
     go_to_XY(x, y);
-    printf("%c", Code);
+    printf("%c", Codigo);
 }
 
-
-   /////////////////////////////
-/////// DRAW FUNCTIONS //////////
-  //////////////////////////////
-
-void draw_title() 
+void dibujar_titulo()
 {
     int title_length = strlen("SIMON DICE: JUEGO");
-    int title_x = (50 - title_length) / 2; 
+    int title_x = (50 - title_length) / 2;
     specialchar_to_XY(0, 0, 201);
-    specialchar_to_XY(49, 0, 187); 
+    specialchar_to_XY(49, 0, 187);
     specialchar_to_XY(0, 4, 200);
-    specialchar_to_XY(49, 4, 188); 
+    specialchar_to_XY(49, 4, 188);
 
     text_to_XY(title_x, 2, "SIMON DICE: JUEGO");
-    
-    for(int i = 0; i < 48; i++) 
+
+    for (int i = 0; i < 48; i++)
         specialchar_to_XY(i + 1, 0, 205);
 
-    for(int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; i++)
+    {
         specialchar_to_XY(0, i + 1, 186);
-        specialchar_to_XY(49, i + 1, 186); 
+        specialchar_to_XY(49, i + 1, 186);
     }
 
-    for(int i = 0; i < 48; i++) 
+    for (int i = 0; i < 48; i++)
         specialchar_to_XY(i + 1, 4, 205);
 
     printf("\n");
 }
 
-void draw_wasd() {
-    // w
-    specialchar_to_XY(21, 6, 219);
-    specialchar_to_XY(28, 6, 219);
-
-    specialchar_to_XY(21, 7, 219);
-    specialchar_to_XY(28, 7, 219);
-
-    specialchar_to_XY(21, 8, 219);
-    specialchar_to_XY(28, 8, 219);
-
-    text_to_XY(24, 7, "W");
-
-    // a
-    specialchar_to_XY(12, 10, 219);
-    specialchar_to_XY(19, 10, 219);
-    specialchar_to_XY(12, 11, 219);
-    specialchar_to_XY(19, 11, 219);
-    specialchar_to_XY(12, 12, 219);
-    specialchar_to_XY(19, 12, 219);
-    text_to_XY(15, 11, "A");
-
-    // s
+void dibujar_botones()
+{
+    // 8
+    specialchar_to_XY(21, 9, 219);
+    specialchar_to_XY(28, 9, 219);
     specialchar_to_XY(21, 10, 219);
     specialchar_to_XY(28, 10, 219);
     specialchar_to_XY(21, 11, 219);
     specialchar_to_XY(28, 11, 219);
-    specialchar_to_XY(21, 12, 219);
-    specialchar_to_XY(28, 12, 219);
-    text_to_XY(24, 11, "S");
+    text_to_XY(24, 10, "8");
 
-    // d
-    specialchar_to_XY(30, 10, 219);
-    specialchar_to_XY(37, 10, 219);
-    specialchar_to_XY(30, 11, 219);
-    specialchar_to_XY(37, 11, 219);
-    specialchar_to_XY(30, 12, 219);
-    specialchar_to_XY(37, 12, 219);
-    text_to_XY(33, 11, "D");
+    // 4
+    specialchar_to_XY(12, 13, 219);
+    specialchar_to_XY(19, 13, 219);
+    specialchar_to_XY(12, 14, 219);
+    specialchar_to_XY(19, 14, 219);
+    specialchar_to_XY(12, 15, 219);
+    specialchar_to_XY(19, 15, 219);
+    text_to_XY(15, 14, "4");
+
+    // 6
+    specialchar_to_XY(30, 13, 219);
+    specialchar_to_XY(37, 13, 219);
+    specialchar_to_XY(30, 14, 219);
+    specialchar_to_XY(37, 14, 219);
+    specialchar_to_XY(30, 15, 219);
+    specialchar_to_XY(37, 15, 219);
+    text_to_XY(33, 14, "6");
+
+    // 2
+    specialchar_to_XY(21, 17, 219);
+    specialchar_to_XY(28, 17, 219);
+    specialchar_to_XY(21, 18, 219);
+    specialchar_to_XY(28, 18, 219);
+    specialchar_to_XY(21, 19, 219);
+    specialchar_to_XY(28, 19, 219);
+    text_to_XY(24, 18, "2");
 }
 
-
-void draw_color(int letter_code) {
-    
-    char letter;
-
-    switch(letter_code)
+void dibujar_color(int cod)
+{
+    switch (cod)
     {
-        case 0: //w
-            for(int i = 22; i < 28; i++)
-            {
-                go_to_XY(i, 6);
-                printf(AMARILLO_F " " RESET_COLOR);
-            }
-
-            for(int i = 22; i < 28; i++)
-            {
-                go_to_XY(i, 8);
-                printf(AMARILLO_F " " RESET_COLOR);
-            }      
+    case 0:
+        for (int i = 21; i <= 28; i++)
+        {
+            go_to_XY(i, 9);
+            printf(NEGRO_F " " RESET_COLOR);
+        }
+        for (int i = 21; i <= 28; i++)
+        {
+            go_to_XY(i, 11);
+            printf(NEGRO_F " " RESET_COLOR);
+        }
+        for (int i = 12; i <= 19; i++)
+        {
+            go_to_XY(i, 13);
+            printf(NEGRO_F " " RESET_COLOR);
+            go_to_XY(i, 15);
+            printf(NEGRO_F " " RESET_COLOR);
+        }
+        for (int i = 30; i <= 37; i++)
+        {
+            go_to_XY(i, 13);
+            printf(NEGRO_F " " RESET_COLOR);
+            go_to_XY(i, 15);
+            printf(NEGRO_F " " RESET_COLOR);
+        }
+        for (int i = 21; i <= 28; i++)
+        {
+            go_to_XY(i, 17);
+            printf(NEGRO_F " " RESET_COLOR);
+            go_to_XY(i, 19);
+            printf(NEGRO_F " " RESET_COLOR);
+        }
         break;
-
-        case 1: //a
-            for(int i = 13; i < 19; i++)
-            {
-                go_to_XY(i, 10);
-                printf(ROJO_F " " RESET_COLOR);
-            }
-
-            for(int i = 13; i < 19; i++)
-            {
-                go_to_XY(i, 12);
-                printf(ROJO_F " " RESET_COLOR);
-            }      
+    case 8:
+        for (int i = 21; i <= 28; i++)
+        {
+            go_to_XY(i, 9);
+            printf(AZUL_F " " RESET_COLOR);
+        }
+        for (int i = 21; i <= 28; i++)
+        {
+            go_to_XY(i, 11);
+            printf(AZUL_F " " RESET_COLOR);
+        }
         break;
-
-        case 2: //s
-            for(int i = 22; i < 28; i++)
-            {
-                go_to_XY(i, 10);
-                printf(AZUL_F " " RESET_COLOR);
-            }
-
-            for(int i = 22; i < 28; i++)
-            {
-                go_to_XY(i, 12);
-                printf(AZUL_F " " RESET_COLOR);
-            } 
+    case 4:
+        for (int i = 12; i <= 19; i++)
+        {
+            go_to_XY(i, 13);
+            printf(AMARILLO_F " " RESET_COLOR);
+            go_to_XY(i, 15);
+            printf(AMARILLO_F " " RESET_COLOR);
+        }
         break;
-
-        case 3: //d
-            for(int i = 31; i < 37; i++)
-            {
-                go_to_XY(i, 10);
-                printf(MAGENTA_F " " RESET_COLOR);
-            }
-
-            for(int i = 31; i < 37; i++)
-            {
-                go_to_XY(i, 12);
-                printf(MAGENTA_F " " RESET_COLOR);
-            } 
+    case 6:
+        for (int i = 30; i <= 37; i++)
+        {
+            go_to_XY(i, 13);
+            printf(ROJO_F " " RESET_COLOR);
+            go_to_XY(i, 15);
+            printf(ROJO_F " " RESET_COLOR);
+        }
+        break;
+    case 2:
+        for (int i = 21; i <= 28; i++)
+        {
+            go_to_XY(i, 17);
+            printf(MAGENTA_F " " RESET_COLOR);
+            go_to_XY(i, 19);
+            printf(MAGENTA_F " " RESET_COLOR);
+        }
         break;
     }
-
-    draw_wasd();
-
-   
 }
 
-
-
-   /////////////////////////////
-/////// GAME FUNCTIONS //////////
-  //////////////////////////////
-
-
-
-//****************
 int msges()
-{ int op;
-  printf("\n");
-  printf ("   M  E   N   U \n");
-  printf("1.- JUGAR \n");
-  printf("0.- SALIR  \n");
-  printf("ESCOGE UNA OPCION: ");
-  scanf ("%d",&op);
-  return op;
-}
-
-
-//****************
-void menu()
-{
-    int vector[] = {};
-    int points;
-    int op;
-
-    op=msges();
-    
-    switch (op)
-    {
-        case 0:
-        gameover(0);
-              break;
-        
-        case 1:
-        play(vector, points);
-              break;
-    }
-
-}
-
-
-/***************************************/
-void gameover(int status)
-{
-    switch(status)
-    {
-        case 0:
-            printf("\n GAMEOVER. \n");
-        break;
-    }
-}
-
-
-/***************************************/
-
-void cls()
 {
     system("cls");
+    int op;
+    printf("\n");
+    printf("   M  E   N   U \n");
+    printf("1.- JUGAR \n");
+    printf("0.- SALIR  \n");
+    printf("ESCOGE UNA OPCION: ");
+    scanf("%d", &op);
+    return op;
 }
 
-
-/***************************************/
-
-void pause()
+void menu()
 {
-    system("pause");
+    int op;
+
+    op = msges();
+
+    switch (op)
+    {
+    case 1:
+        juego();
+        break;
+    }
 }
 
-
-/***************************************/
-
-void play(int vector[], int points)
+void juego()
 {
-    int bandera = 1;
+    system("cls");
     int aleatorio;
-    int i = 0;
-    char ch;
+    int secuencia_original[100];
+    int elementos_original = 0;
+    int secuencia_usuario[100];
+    int elementos_usuario = 0;
+    int puntos = 0;
+    int vida = 1;
+    int secuencias_iguales = 1;
 
-    cls();
-    draw_title();
-    draw_wasd();
-    printf("\n\n\n");
+    dibujar_titulo();
+    dibujar_botones();
 
     do
     {
-        aleatorio = rand() % 4;
-        sleep(1);
-        draw_color(aleatorio);
-        vector[i] = aleatorio;
-        i++;
+        secuencia_original[elementos_original] = aleat();
 
-        for(int j = i; j <= 0; j--)
+        if (elementos_original > 0)
         {
-            printf("\n\n\n\n\n\n\n Introduce el orden:");
-            fflush(stdin);
-            gets(ch);
+            for (int i = 0; i < elementos_original; i++)
+            {
+                dibujar_color(secuencia_original[i]);
+                sleep(1);
+                dibujar_color(0);
+            }
+        }
+        else
+        {
+            dibujar_color(secuencia_original[0]);
         }
 
+        sleep(1);
+        dibujar_botones();
 
-    }while(bandera);
+        printf("\n\n\n\n");
 
-    printf("\n\n\n\n\n\n");
+        printf("\nINGRESA LA SECUENCIA ->: \n");
 
+        for (int i = 0; i < elementos_original; i++)
+        {
+            scanf("%d", &secuencia_usuario[i]);
+        }
+
+        for (int i = 0; i < elementos_original; i++)
+        {
+            if (secuencia_usuario[i] != secuencia_original[i])
+            {
+                secuencias_iguales = 0;
+                break;
+            }
+        }
+
+        if (secuencias_iguales == 0)
+        {
+            vida = 0;
+            printf("\n FIN DEL JUEGO \n");
+        }
+        else
+        {
+            elementos_usuario++;
+            elementos_original++;
+            puntos++;
+        }
+    } while (vida);
+
+    printf("\n PUNTOS: %d\n\n", (puntos-1));
+    system("pause");
 }
 
-
+int aleat()
+{
+    int permitidos[4] = {8, 4, 6, 2};
+    int aleatorio = rand() % 4;
+    return permitidos[aleatorio];
+}
